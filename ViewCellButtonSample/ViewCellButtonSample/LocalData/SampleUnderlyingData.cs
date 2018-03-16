a40256e9580ca1d82207e2ba65e58447ca377c3c
ac <andrew.chung@xamarin.com>
2018-03-15 17:20:06 -0700

    second checkin
    second checkin

--- ViewCellButtonSample/ViewCellButtonSample/LocalData/SampleUnderlyingData.cs
+++ ViewCellButtonSample/ViewCellButtonSample/LocalData/SampleUnderlyingData.cs
@@ -33,7 +33,55 @@ namespace ViewCellButtonSample.LocalData
                 CreatedDateTimeString = DateTime.Now.ToString("MMM d h:mm tt", new CultureInfo("en-US")),
                 CreatedDateTime = DateTimeOffset.UtcNow,
                 TheChangingString = "first test string"
-            }
+            },
+            new UnderlyingData (){
+                Id = 5,
+                CreatedDateTimeString = DateTime.Now.ToString("MMM d h:mm tt", new CultureInfo("en-US")),
+                CreatedDateTime = DateTimeOffset.UtcNow,
+                TheChangingString = "first test string"
+            },
+            new UnderlyingData (){
+                Id = 6,
+                CreatedDateTimeString = DateTime.Now.ToString("MMM d h:mm tt", new CultureInfo("en-US")),
+                CreatedDateTime = DateTimeOffset.UtcNow,
+                TheChangingString = "first test string"
+            },
+            new UnderlyingData (){
+                Id = 7,
+                CreatedDateTimeString = DateTime.Now.ToString("MMM d h:mm tt", new CultureInfo("en-US")),
+                CreatedDateTime = DateTimeOffset.UtcNow,
+                TheChangingString = "first test string"
+            },
+            new UnderlyingData (){
+                Id = 8,
+                CreatedDateTimeString = DateTime.Now.ToString("MMM d h:mm tt", new CultureInfo("en-US")),
+                CreatedDateTime = DateTimeOffset.UtcNow,
+                TheChangingString = "first test string"
+            },
+            new UnderlyingData (){
+                Id = 9,
+                CreatedDateTimeString = DateTime.Now.ToString("MMM d h:mm tt", new CultureInfo("en-US")),
+                CreatedDateTime = DateTimeOffset.UtcNow,
+                TheChangingString = "first test string"
+            },
+            new UnderlyingData (){
+                Id = 10,
+                CreatedDateTimeString = DateTime.Now.ToString("MMM d h:mm tt", new CultureInfo("en-US")),
+                CreatedDateTime = DateTimeOffset.UtcNow,
+                TheChangingString = "first test string"
+            },
+            new UnderlyingData (){
+                Id = 11,
+                CreatedDateTimeString = DateTime.Now.ToString("MMM d h:mm tt", new CultureInfo("en-US")),
+                CreatedDateTime = DateTimeOffset.UtcNow,
+                TheChangingString = "first test string"
+            },
+            new UnderlyingData (){
+                Id = 12,
+                CreatedDateTimeString = DateTime.Now.ToString("MMM d h:mm tt", new CultureInfo("en-US")),
+                CreatedDateTime = DateTimeOffset.UtcNow,
+                TheChangingString = "first test string"
+            },
         };
     }
 
--- ViewCellButtonSample/ViewCellButtonSample/Models/UnderlyingData.cs
+++ ViewCellButtonSample/ViewCellButtonSample/Models/UnderlyingData.cs
@@ -10,20 +10,4 @@ namespace ViewCellButtonSample.Models
         public string TheChangingString { get; set; }
     }
 
-    ////LOCAL-COSMOSDB
-    //public class CosmosDBPrayerRequest
-    //{
-    //    public string Id { get; set; }
-    //    public string CreatedDateTimeString { get; set; }
-
-    //    public DateTimeOffset CreatedDateTime { get; set; }
-
-    //    public string FirstName { get; set; }
-    //    public string LastName { get; set; }
-    //    public string FullName { get; set; }
-    //    public string FBProfileUrl { get; set; }
-    //    public string PrayerRequestText { get; set; }
-    //    public int NumberOfThoughts { get; set; }
-    //    public int NumberOfPrayers { get; set; }
-    //}
 }
